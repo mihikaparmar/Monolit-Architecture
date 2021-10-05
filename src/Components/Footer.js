cf48af7806c7239ecfc291ccb8c32f9e532afef7
Mihika Parmar <80045642+mihikaparmar@users.noreply.github.com>
2021-10-05 10:07:38 +0530

    first commit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,20 +3,20 @@ import React from 'react'
 export default function Footer() {
     return (
         <div class="footer">
-            <br/>
-            <br/>
-            <br/>
+            <br />
+            <br />
+            <br />
             <footer class="w3-container w3-padding-64 w3-center w3-black w3-xlarge">
-  <a href="#"><i class="fa fa-facebook-official"></i></a>
-  <a href="#"><i class="fa fa-pinterest-p"></i></a>
-  <a href="#"><i class="fa fa-twitter"></i></a>
-  
-  <a href="#"><i class="fa fa-linkedin"></i></a>
-  <p class="w3-medium">
-  Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">Monolit Architecture</a>
-  </p>
-</footer>
-<br/>
+                <a href="#"><i class="fa fa-facebook-official"></i></a>
+                <a href="#"><i class="fa fa-pinterest-p"></i></a>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+
+                <a href="#"><i class="fa fa-linkedin"></i></a>
+                <p class="w3-medium">
+                    Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">Monolit Architecture</a>
+                </p>
+            </footer>
+            <br />
 
         </div>
     )
